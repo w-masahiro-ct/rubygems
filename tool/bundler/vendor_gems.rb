4e02243f662b74a03c57dc5a4fffe05dd203d57b
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2025-11-17 14:18:55 +0900

    Update optparse-0.8.0

--- tool/bundler/vendor_gems.rb
+++ tool/bundler/vendor_gems.rb
@@ -7,7 +7,7 @@ gem "molinillo", github: "cocoapods/molinillo", ref: "1d62d7d5f448e79418716dc779
 gem "net-http", "0.8.0"
 gem "net-http-persistent", "4.0.6"
 gem "net-protocol", "0.2.2"
-gem "optparse", "0.6.0"
+gem "optparse", "0.8.0"
 gem "pub_grub", github: "jhawthorn/pub_grub", ref: "df6add45d1b4d122daff2f959c9bd1ca93d14261"
 gem "resolv", "0.6.2"
 gem "securerandom", "0.4.1"
