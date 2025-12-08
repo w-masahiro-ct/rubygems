w-masahiro-ct/rubygems

commit 573ffad3ea4a8d364b0f9bf3a18f0d5bf1197e38

Complete remembered options removal

bundler/lib/bundler/endpoint_specification.rb
@@ -60,28 +60,6 @@ module Bundler
       end
     end
 
-    # needed for binstubs
-    def executables
-      if @remote_specification
-        @remote_specification.executables
-      elsif _local_specification
-        _local_specification.executables
-      else
-        super
-      end
-    end
-
-    # needed for bundle clean
-    def bindir
-      if @remote_specification
-        @remote_specification.bindir
-      elsif _local_specification
-        _local_specification.bindir
-      else
-        super
-      end
-    end
-
     # needed for post_install_messages during install
     def post_install_message
       if @remote_specification

bundler/spec/commands/install_spec.rb
@@ -340,20 +340,6 @@ RSpec.describe "bundle install with gem sources" do
         bundle "install"
         expect(the_bundle).to include_gems "myrack 1.0"
       end
-
-      it "allows running bundle install --system without deleting foo" do
-        bundle "install --path vendor"
-        bundle "install --system"
-        FileUtils.rm_r(bundled_app("vendor"))
-        expect(the_bundle).to include_gems "myrack 1.0"
-      end
-
-      it "allows running bundle install --system after deleting foo" do
-        bundle "install --path vendor"
-        FileUtils.rm_r(bundled_app("vendor"))
-        bundle "install --system"
-        expect(the_bundle).to include_gems "myrack 1.0"
-      end
     end
 
     it "finds gems in multiple sources" do

bundler/lib/bundler/cli.rb
@@ -173,7 +173,7 @@ module Bundler
     D
     method_option "dry-run", type: :boolean, default: false, banner: "Lock the Gemfile"
     method_option "gemfile", type: :string, banner: "Use the specified gemfile instead of Gemfile"
-    method_option "path", type: :string, banner: "Specify a different path than the system default ($BUNDLE_PATH or $GEM_HOME).#{" Bundler will remember this value for future installs on this machine" unless Bundler.feature_flag.forget_cli_options?}"
+    method_option "path", type: :string, banner: "Specify a different path than the system default, namely, $BUNDLE_PATH or $GEM_HOME (removed)"
     def check
       remembered_flag_deprecation("path")
 
@@ -211,9 +211,9 @@ module Bundler
       If the bundle has already been installed, bundler will tell you so and then exit.
     D
     method_option "binstubs", type: :string, lazy_default: "bin", banner: "Generate bin stubs for bundled gems to ./bin"
-    method_option "clean", type: :boolean, banner: "Run bundle clean automatically after install"
-    method_option "deployment", type: :boolean, banner: "Install using defaults tuned for deployment environments"
-    method_option "frozen", type: :boolean, banner: "Do not allow the Gemfile.lock to be updated after this install"
+    method_option "clean", type: :boolean, banner: "Run bundle clean automatically after install (removed)"
+    method_option "deployment", type: :boolean, banner: "Install using defaults tuned for deployment environments (removed)"
+    method_option "frozen", type: :boolean, banner: "Do not allow the Gemfile.lock to be updated after this install (removed)"
     method_option "full-index", type: :boolean, banner: "Fall back to using the single-file index of all gems"
     method_option "gemfile", type: :string, banner: "Use the specified gemfile instead of Gemfile"
     method_option "jobs", aliases: "-j", type: :numeric, banner: "Specify the number of jobs to run in parallel"
@@ -221,16 +221,16 @@ module Bundler
     method_option "prefer-local", type: :boolean, banner: "Only attempt to fetch gems remotely if not present locally, even if newer versions are available remotely"
     method_option "no-cache", type: :boolean, banner: "Don't update the existing gem cache."
     method_option "force", type: :boolean, aliases: "--redownload", banner: "Force reinstalling every gem, even if already installed"
-    method_option "no-prune", type: :boolean, banner: "Don't remove stale gems from the cache."
-    method_option "path", type: :string, banner: "Specify a different path than the system default ($BUNDLE_PATH or $GEM_HOME).#{" Bundler will remember this value for future installs on this machine" unless Bundler.feature_flag.forget_cli_options?}"
+    method_option "no-prune", type: :boolean, banner: "Don't remove stale gems from the cache (removed)."
+    method_option "path", type: :string, banner: "Specify a different path than the system default, namely, $BUNDLE_PATH or $GEM_HOME (removed)."
     method_option "quiet", type: :boolean, banner: "Only output warnings and errors."
-    method_option "shebang", type: :string, banner: "Specify a different shebang executable name than the default (usually 'ruby')"
+    method_option "shebang", type: :string, banner: "Specify a different shebang executable name than the default, usually 'ruby' (removed)"
     method_option "standalone", type: :array, lazy_default: [], banner: "Make a bundle that can work without the Bundler runtime"
-    method_option "system", type: :boolean, banner: "Install to the system location ($BUNDLE_PATH or $GEM_HOME) even if the bundle was previously installed somewhere else for this application"
+    method_option "system", type: :boolean, banner: "Install to the system location ($BUNDLE_PATH or $GEM_HOME) even if the bundle was previously installed somewhere else for this application (removed)"
     method_option "trust-policy", alias: "P", type: :string, banner: "Gem trust policy (like gem install -P). Must be one of #{Bundler.rubygems.security_policy_keys.join("|")}"
     method_option "target-rbconfig", type: :string, banner: "Path to rbconfig.rb for the deployment target platform"
-    method_option "without", type: :array, banner: "Exclude gems that are part of the specified named group."
-    method_option "with", type: :array, banner: "Include gems that are part of the specified named group."
+    method_option "without", type: :array, banner: "Exclude gems that are part of the specified named group (removed)."
+    method_option "with", type: :array, banner: "Include gems that are part of the specified named group (removed)."
     def install
       %w[clean deployment frozen no-prune path shebang without with].each do |option|
         remembered_flag_deprecation(option)
@@ -323,7 +323,7 @@ module Bundler
       will create binstubs for all given gems.
     D
     method_option "force", type: :boolean, default: false, banner: "Overwrite existing binstubs if they exist"
-    method_option "path", type: :string, lazy_default: "bin", banner: "Binstub destination directory (default bin)"
+    method_option "path", type: :string, lazy_default: "bin", banner: "Binstub destination directory, `bin` by default (removed)"
     method_option "shebang", type: :string, banner: "Specify a different shebang executable name than the default (usually 'ruby')"
     method_option "standalone", type: :boolean, banner: "Make binstubs that can work without the Bundler runtime"
     method_option "all", type: :boolean, banner: "Install binstubs for all gems"
@@ -396,15 +396,15 @@ module Bundler
     end
 
     desc "cache [OPTIONS]", "Locks and then caches all of the gems into vendor/cache"
-    method_option "all", type: :boolean, default: Bundler.feature_flag.cache_all?, banner: "Include all sources (including path and git)."
+    method_option "all", type: :boolean, default: Bundler.settings[:cache_all], banner: "Include all sources (including path and git) (removed)."
     method_option "all-platforms", type: :boolean, banner: "Include gems for all platforms present in the lockfile, not only the current one"
     method_option "cache-path", type: :string, banner: "Specify a different cache path than the default (vendor/cache)."
     method_option "gemfile", type: :string, banner: "Use the specified gemfile instead of Gemfile"
     method_option "no-install", type: :boolean, banner: "Don't install the gems, only update the cache."
-    method_option "no-prune", type: :boolean, banner: "Don't remove stale gems from the cache."
-    method_option "path", type: :string, banner: "Specify a different path than the system default ($BUNDLE_PATH or $GEM_HOME).#{" Bundler will remember this value for future installs on this machine" unless Bundler.feature_flag.forget_cli_options?}"
+    method_option "no-prune", type: :boolean, banner: "Don't remove stale gems from the cache (removed)."
+    method_option "path", type: :string, banner: "Specify a different path than the system default, namely, $BUNDLE_PATH or $GEM_HOME (removed)."
     method_option "quiet", type: :boolean, banner: "Only output warnings and errors."
-    method_option "frozen", type: :boolean, banner: "Do not allow the Gemfile.lock to be updated after this bundle cache operation's install"
+    method_option "frozen", type: :boolean, banner: "Do not allow the Gemfile.lock to be updated after this bundle cache operation's install (removed)"
     long_desc <<-D
       The cache command will copy the .gem files for every gem in the bundle into the
       directory ./vendor/cache. If you then check that directory into your source
@@ -420,17 +420,12 @@ module Bundler
       end
 
       if flag_passed?("--path")
-        message =
-          "The `--path` flag is deprecated because its semantics are unclear. " \
-          "Use `bundle config cache_path` to configure the path of your cache of gems, " \
-          "and `bundle config path` to configure the path where your gems are installed, " \
-          "and stop using this flag"
         removed_message =
           "The `--path` flag has been removed because its semantics were unclear. " \
           "Use `bundle config cache_path` to configure the path of your cache of gems, " \
           "and `bundle config path` to configure the path where your gems are installed, " \
           "and stop using this flag"
-        SharedHelpers.major_deprecation 2, message, removed_message: removed_message
+        raise InvalidOption, removed_message
       end
 
       require_relative "cli/cache"
@@ -750,17 +745,12 @@ module Bundler
     end
 
     def print_remembered_flag_deprecation(flag_name, option_name, option_value)
-      message =
-        "The `#{flag_name}` flag is deprecated because it relies on being " \
-        "remembered across bundler invocations, which bundler will no longer " \
-        "do in future versions. Instead please use `bundle config set #{option_name} " \
-        "#{option_value}`, and stop using this flag"
       removed_message =
         "The `#{flag_name}` flag has been removed because it relied on being " \
         "remembered across bundler invocations, which bundler no longer does. " \
         "Instead please use `bundle config set #{option_name} #{option_value}`, " \
         "and stop using this flag"
-      Bundler::SharedHelpers.major_deprecation 2, message, removed_message: removed_message
+      raise InvalidOption, removed_message
     end
 
     def flag_passed?(name)

bundler/lib/bundler/cli/cache.rb
@@ -10,16 +10,12 @@ module Bundler
 
     def run
       Bundler.ui.level = "warn" if options[:quiet]
-      Bundler.settings.set_command_option_if_given :path, options[:path]
       Bundler.settings.set_command_option_if_given :cache_path, options["cache-path"]
 
-      setup_cache_all
       install
 
-      custom_path = Bundler.settings[:path] if options[:path]
-
       Bundler.settings.temporary(cache_all_platforms: options["all-platforms"]) do
-        Bundler.load.cache(custom_path)
+        Bundler.load.cache
       end
     end
 
@@ -31,12 +27,6 @@ module Bundler
       options["local"] = false if Bundler.settings[:cache_all_platforms]
       options["no-cache"] = true
       Bundler::CLI::Install.new(options).run
-    end
-
-    def setup_cache_all
-      all = options.fetch(:all, Bundler.feature_flag.cache_all? || nil)
-
-      Bundler.settings.set_command_option_if_given :cache_all, all
     end
   end
 end

bundler/lib/bundler/cli/install.rb
@@ -20,36 +20,18 @@ module Bundler
 
       Bundler::SharedHelpers.set_env "RB_USER_INSTALL", "1" if Gem.freebsd_platform?
 
-      # Disable color in deployment mode
-      Bundler.ui.shell = Thor::Shell::Basic.new if options[:deployment]
-
       if target_rbconfig_path = options[:"target-rbconfig"]
         Bundler.rubygems.set_target_rbconfig(target_rbconfig_path)
       end
 
-      check_for_options_conflicts
-
       check_trust_policy
 
-      if options[:deployment] || options[:frozen] || Bundler.frozen_bundle?
-        unless Bundler.default_lockfile.exist?
-          flag = "--deployment flag" if options[:deployment]
-          flag ||= "--frozen flag" if options[:frozen]
-          flag ||= "deployment setting" if Bundler.settings[:deployment]
-          flag ||= "frozen setting" if Bundler.settings[:frozen]
-          raise ProductionError, "The #{flag} requires a lockfile. Please make " \
-                                 "sure you have checked your #{SharedHelpers.relative_lockfile_path} into version control " \
-                                 "before deploying."
-        end
-
-        Bundler.settings.set_command_option :deployment, true if options[:deployment]
-        Bundler.settings.set_command_option :frozen, true if options[:frozen]
-      end
-
-      # When install is called with --no-deployment, disable deployment mode
-      if options[:deployment] == false
-        Bundler.settings.set_command_option :frozen, nil
-        options[:system] = true
+      if Bundler.frozen_bundle? && !Bundler.default_lockfile.exist?
+        flag = "deployment setting" if Bundler.settings[:deployment]
+        flag = "frozen setting" if Bundler.settings[:frozen]
+        raise ProductionError, "The #{flag} requires a lockfile. Please make " \
+                               "sure you have checked your #{SharedHelpers.relative_lockfile_path} into version control " \
+                               "before deploying."
       end
 
       normalize_settings
@@ -118,22 +100,6 @@ module Bundler
       "#{count} #{count == 1 ? "gem" : "gems"} now installed"
     end
 
-    def check_for_group_conflicts_in_cli_options
-      conflicting_groups = Array(options[:without]) & Array(options[:with])
-      return if conflicting_groups.empty?
-      raise InvalidOption, "You can't list a group in both with and without." \
-        " The offending groups are: #{conflicting_groups.join(", ")}."
-    end
-
-    def check_for_options_conflicts
-      if (options[:path] || options[:deployment]) && options[:system]
-        error_message = String.new
-        error_message << "You have specified both --path as well as --system. Please choose only one option.\n" if options[:path]
-        error_message << "You have specified both --deployment as well as --system. Please choose only one option.\n" if options[:deployment]
-        raise InvalidOption.new(error_message)
-      end
-    end
-
     def check_trust_policy
       trust_policy = options["trust-policy"]
       unless Bundler.rubygems.security_policies.keys.unshift(nil).include?(trust_policy)
@@ -143,27 +109,10 @@ module Bundler
       Bundler.settings.set_command_option_if_given :"trust-policy", trust_policy
     end
 
-    def normalize_groups
-      check_for_group_conflicts_in_cli_options
-
-      # need to nil them out first to get around validation for backwards compatibility
-      Bundler.settings.set_command_option :without, nil
-      Bundler.settings.set_command_option :with,    nil
-      Bundler.settings.set_command_option :without, options[:without]
-      Bundler.settings.set_command_option :with,    options[:with]
-    end
-
     def normalize_settings
-      Bundler.settings.set_command_option :path, nil if options[:system]
-      Bundler.settings.set_command_option_if_given :path, options[:path]
-
       if options["standalone"] && Bundler.settings[:path].nil? && !options["local"]
         Bundler.settings.set_command_option :path, "bundle"
       end
-
-      bin_option = options["binstubs"]
-      bin_option = nil if bin_option&.empty?
-      Bundler.settings.set_command_option :bin, bin_option if options["binstubs"]
 
       Bundler.settings.set_command_option_if_given :shebang, options["shebang"]
 
@@ -174,8 +123,6 @@ module Bundler
       Bundler.settings.set_command_option_if_given :no_install, options["no-install"]
 
       Bundler.settings.set_command_option_if_given :clean, options["clean"]
-
-      normalize_groups if options[:without] || options[:with]
 
       options[:force] = options[:redownload] if options[:redownload]
     end

bundler/lib/bundler/feature_flag.rb
@@ -29,7 +29,6 @@ module Bundler
 
     settings_flag(:allow_offline_install) { bundler_4_mode? }
     settings_flag(:cache_all) { bundler_4_mode? }
-    settings_flag(:forget_cli_options) { bundler_4_mode? }
     settings_flag(:global_gem_cache) { bundler_5_mode? }
     settings_flag(:lockfile_checksums) { bundler_4_mode? }
     settings_flag(:plugins) { @bundler_version >= Gem::Version.new("1.14") }

bundler/lib/bundler/installer/gem_installer.rb
@@ -16,7 +16,6 @@ module Bundler
     def install_from_spec
       post_install_message = install
       Bundler.ui.debug "#{worker}:  #{spec.name} (#{spec.version}) from #{spec.loaded_from}"
-      generate_executable_stubs
       [true, post_install_message]
     rescue Bundler::InstallHookError, Bundler::SecurityError, Bundler::APIResponseMismatchError, Bundler::InsecureInstallPathError
       raise
@@ -70,16 +69,6 @@ module Bundler
 
     def out_of_space_message
       "#{install_error_message}\nYour disk is out of space. Free some space to be able to install your bundle."
-    end
-
-    def generate_executable_stubs
-      return if Bundler.feature_flag.forget_cli_options?
-      return if Bundler.settings[:inline]
-      if Bundler.settings[:bin] && standalone
-        installer.generate_standalone_bundler_executable_stubs(spec)
-      elsif Bundler.settings[:bin]
-        installer.generate_bundler_executable_stubs(spec, force: true)
-      end
     end
   end
 end

bundler/lib/bundler/man/bundle-add.1
@@ -1,6 +1,6 @@
 .\" generated with Ronn-NG/v0.10.1
 .\" http://github.com/apjanke/ronn-ng/tree/0.10.1
-.TH "BUNDLE\-ADD" "1" "August 2025" ""
+.TH "BUNDLE\-ADD" "1" "September 2025" ""
 .SH "NAME"
 \fBbundle\-add\fR \- Add gem to the Gemfile and run bundle install
 .SH "SYNOPSIS"

commit b47f6b0247ee2e791df4a855d6da009900f4571c

Fix Bundler removing executables after creating them
When running a fresh `bundle install` with gems that contains
executables, Bundler will generate binstubs but soon after will remove
them. This is a regression introduced in 573ffad3ea4a8d364b0f9bf3a18f0d5bf1197e38.

This results in doing `bundle install && bundle exec foo` to raise an
error saying `foo` couldn't be found.

This issue only appears if `BUNDLE_CLEAN` is set.

At the end of the installation process, when Bundler has installed
gems and generated binstubs, it runs the cleanup.

1. It [detects](https://github.com/ruby/rubygems/blob/4f8aa3b40cded3465bb2cd761e9ce7f8673b7fcb/bundler/lib/bundler/runtime.rb#L182) the executable for the current specs
2. Any existing executables not detected is then removed https://github.com/ruby/rubygems/blob/4f8aa3b40cded3465bb2cd761e9ce7f8673b7fcb/bundler/lib/bundler/runtime.rb#L194.

The issue being that 1. now returns an empty array where as it should
return the executables of the gems from the current bundle.

The problem is in 573ffad3ea4a8d364b0f9bf3a18f0d5bf1197e38 where we
removed the `executables` method from the `EndpointSpecification`.
When Bundler reads the lockfile, it creates a `EndpointSpecification`
object for each spec. At this point, the EndpointSpecification doesn't
know about the `executables` of a gem. Once Bundler fetches the
`gemspec` from the remote, it swaps the the "spec" with the real one
and from here knows what executables the gem has.

Reintroduce the `executables` method and the `bindir` in the
EndpointSpecification class. From what I'm seeing, the removal
of those wasn't needed to resolve the issue where Bundler remembers
CLI flags. This is probably an oversight.

bundler/lib/bundler/endpoint_specification.rb
@@ -60,6 +60,28 @@ module Bundler
       end
     end
 
+    # needed for binstubs
+    def executables
+      if @remote_specification
+        @remote_specification.executables
+      elsif _local_specification
+        _local_specification.executables
+      else
+        super
+      end
+    end
+
+    # needed for bundle clean
+    def bindir
+      if @remote_specification
+        @remote_specification.bindir
+      elsif _local_specification
+        _local_specification.bindir
+      else
+        super
+      end
+    end
+
     # needed for post_install_messages during install
     def post_install_message
       if @remote_specification

bundler/spec/commands/install_spec.rb
@@ -1885,6 +1885,25 @@ RSpec.describe "bundle install with gem sources" do
     expect(Dir.glob(vendored_gems("bin/*"))).to eq(expected_executables)
   end
 
+  it "prevents removing binstubs when BUNDLE_CLEAN is set" do
+    build_repo4 do
+      build_gem "kamal", "4.0.6" do |s|
+        s.executables = ["kamal"]
+      end
+    end
+
+    gemfile = <<~G
+      source "https://gem.repo4"
+      gem "kamal"
+    G
+
+    install_gemfile(gemfile, env: { "BUNDLE_CLEAN" => "true", "BUNDLE_PATH" => "vendor/bundle" })
+
+    expected_executables = [vendored_gems("bin/kamal").to_s]
+    expected_executables << vendored_gems("bin/kamal.bat").to_s if Gem.win_platform?
+    expect(Dir.glob(vendored_gems("bin/*"))).to eq(expected_executables)
+  end
+
   it "preserves lockfile versions conservatively" do
     build_repo4 do
       build_gem "mypsych", "4.0.6" do |s|