w-masahiro-ct/rubygems

commit a555fd6ccd1765c9284349515d585a01866ab406

Allow bundle pristine to run in parallel
Also fix running when BUNDLE_NO_INSTALL happens to be set, same as with install/update commands

bundler/lib/bundler/cli/pristine.rb
@@ -12,40 +12,48 @@ module Bundler
       definition.validate_runtime!
       installer = Bundler::Installer.new(Bundler.root, definition)
 
-      Bundler.load.specs.each do |spec|
-        next if spec.name == "bundler" # Source::Rubygems doesn't install bundler
-        next if !@gems.empty? && !@gems.include?(spec.name)
+      ProcessLock.lock do
+        installed_specs = definition.specs.reject do |spec|
+          next if spec.name == "bundler" # Source::Rubygems doesn't install bundler
+          next if !@gems.empty? && !@gems.include?(spec.name)
 
-        gem_name = "#{spec.name} (#{spec.version}#{spec.git_version})"
-        gem_name += " (#{spec.platform})" if !spec.platform.nil? && spec.platform != Gem::Platform::RUBY
+          gem_name = "#{spec.name} (#{spec.version}#{spec.git_version})"
+          gem_name += " (#{spec.platform})" if !spec.platform.nil? && spec.platform != Gem::Platform::RUBY
 
-        case source = spec.source
-        when Source::Rubygems
-          cached_gem = spec.cache_file
-          unless File.exist?(cached_gem)
-            Bundler.ui.error("Failed to pristine #{gem_name}. Cached gem #{cached_gem} does not exist.")
+          case source = spec.source
+          when Source::Rubygems
+            cached_gem = spec.cache_file
+            unless File.exist?(cached_gem)
+              Bundler.ui.error("Failed to pristine #{gem_name}. Cached gem #{cached_gem} does not exist.")
+              next
+            end
+
+            FileUtils.rm_rf spec.full_gem_path
+          when Source::Git
+            if source.local?
+              Bundler.ui.warn("Cannot pristine #{gem_name}. Gem is locally overridden.")
+              next
+            end
+
+            source.remote!
+            if extension_cache_path = source.extension_cache_path(spec)
+              FileUtils.rm_rf extension_cache_path
+            end
+            FileUtils.rm_rf spec.extension_dir
+            FileUtils.rm_rf spec.full_gem_path
+          else
+            Bundler.ui.warn("Cannot pristine #{gem_name}. Gem is sourced from local path.")
             next
           end
 
-          FileUtils.rm_rf spec.full_gem_path
-        when Source::Git
-          if source.local?
-            Bundler.ui.warn("Cannot pristine #{gem_name}. Gem is locally overridden.")
-            next
-          end
+          true
+        end.map(&:name)
 
-          source.remote!
-          if extension_cache_path = source.extension_cache_path(spec)
-            FileUtils.rm_rf extension_cache_path
-          end
-          FileUtils.rm_rf spec.extension_dir
-          FileUtils.rm_rf spec.full_gem_path
-        else
-          Bundler.ui.warn("Cannot pristine #{gem_name}. Gem is sourced from local path.")
-          next
-        end
-
-        Bundler::GemInstaller.new(spec, installer, false, 0, true).install_from_spec
+        jobs = installer.send(:installation_parallelization, {})
+        pristine_count = definition.specs.count - installed_specs.count
+        # allow a pristining a single gem to skip the parallel worker
+        jobs = [jobs, pristine_count].min
+        ParallelInstaller.call(installer, definition.specs, jobs, false, true, skip: installed_specs)
       end
     end
   end

bundler/lib/bundler/cli.rb
@@ -660,7 +660,9 @@ module Bundler
     D
     def pristine(*gems)
       require_relative "cli/pristine"
-      Pristine.new(gems).run
+      Bundler.settings.temporary(no_install: false) do
+        Pristine.new(gems).run
+      end
     end
 
     if Bundler.feature_flag.plugins?

bundler/lib/bundler/installer/parallel_installer.rb
@@ -62,20 +62,23 @@ module Bundler
       end
     end
 
-    def self.call(*args)
-      new(*args).call
+    def self.call(*args, **kwargs)
+      new(*args, **kwargs).call
     end
 
     attr_reader :size
 
-    def initialize(installer, all_specs, size, standalone, force)
+    def initialize(installer, all_specs, size, standalone, force, skip: nil)
       @installer = installer
       @size = size
       @standalone = standalone
       @force = force
       @specs = all_specs.map {|s| SpecInstallation.new(s) }
+      @specs.each do |spec_install|
+        spec_install.state = :installed if skip.include?(spec_install.name)
+      end if skip
       @spec_set = all_specs
-      @rake = @specs.find {|s| s.name == "rake" }
+      @rake = @specs.find {|s| s.name == "rake" unless s.installed? }
     end
 
     def call

commit 710ba514a8f60d1ea5de72aed1367ddfd0e1ec84

Allow bundle pristine to work for git gems in the same repo:
- Fix #9186
- ### Problem

  Running `bundle pristine` in a Gemfile where there is many git gem
  pointing to the same repository will result in a error
  "Another git process seems to be running in this repository".

  ### Context

  This error is a regression since a555fd6ccd1765c9284349515d585a01866ab406
  where `bundle pristine` now runs in parallel which could lead
  to running simultaneous git operations in the same repository.

  ### Solution

  When Bundler pristine a git gem it does a `git reset --hard` without
  specifying a path.
  This means the whole repository will be reset. In this case, we can
  leverage that by just pristining one gem per unique git sources.
  This is also more efficient.

bundler/lib/bundler/cli/pristine.rb
@@ -11,6 +11,7 @@ module Bundler
       definition = Bundler.definition
       definition.validate_runtime!
       installer = Bundler::Installer.new(Bundler.root, definition)
+      git_sources = []
 
       ProcessLock.lock do
         installed_specs = definition.specs.reject do |spec|
@@ -41,6 +42,9 @@ module Bundler
             end
             FileUtils.rm_rf spec.extension_dir
             FileUtils.rm_rf spec.full_gem_path
+
+            next if git_sources.include?(source)
+            git_sources << source
           else
             Bundler.ui.warn("Cannot pristine #{gem_name}. Gem is sourced from local path.")
             next

bundler/spec/commands/pristine_spec.rb
@@ -89,6 +89,66 @@ RSpec.describe "bundle pristine" do
       expect(changes_txt).to be_file
       expect(err).to include("Cannot pristine #{spec.name} (#{spec.version}#{spec.git_version}). Gem is locally overridden.")
     end
+
+    it "doesn't run multiple git processes for the same repository" do
+      nested_gems = [
+        "actioncable",
+        "actionmailer",
+        "actionpack",
+        "actionview",
+        "activejob",
+        "activemodel",
+        "activerecord",
+        "activestorage",
+        "activesupport",
+        "railties",
+      ]
+
+      build_repo2 do
+        nested_gems.each do |gem|
+          build_lib gem, path: lib_path("rails/#{gem}")
+        end
+
+        build_git "rails", path: lib_path("rails") do |s|
+          nested_gems.each do |gem|
+            s.add_dependency gem
+          end
+        end
+      end
+
+      install_gemfile <<-G
+        source 'https://rubygems.org'
+
+        git "#{lib_path("rails")}" do
+          gem "rails"
+          gem "actioncable"
+          gem "actionmailer"
+          gem "actionpack"
+          gem "actionview"
+          gem "activejob"
+          gem "activemodel"
+          gem "activerecord"
+          gem "activestorage"
+          gem "activesupport"
+          gem "railties"
+        end
+      G
+
+      changed_files = []
+      diff = "#Pristine spec changes"
+
+      nested_gems.each do |gem|
+        spec = find_spec(gem)
+        changed_files << Pathname.new(spec.full_gem_path).join("lib/#{gem}.rb")
+        File.open(changed_files.last, "a") {|f| f.puts diff }
+      end
+
+      bundle "pristine"
+
+      changed_files.each do |changed_file|
+        expect(File.read(changed_file)).to_not include(diff)
+      end
+    end
   end
 
   context "when sourced from gemspec" do